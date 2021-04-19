9182be25a58e078c874f0dfb2d49d1a8538d4f83
inkypaper <inkypaper@protonmail.com>
2021-04-19 14:13:26 +0200

    fix plyr

--- player/script.js
+++ player/script.js
@@ -2,4 +2,5 @@
 // https://github.com/sampotts/plyr/#options
 const player = new Plyr('video', { captions: { active: true } });
 
-// Expose player so it can be used from the console window.player = player;
+// Expose player so it can be used from the console
+window.player = player;
