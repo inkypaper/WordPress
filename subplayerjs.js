a13c8e82079f739f982f8f89b8776e1b844e1300
inkypaper <80489442+inkypaper@users.noreply.github.com>
2021-05-05 14:17:02 +0200

    Update subplayerjs.js

--- subplayerjs.js
+++ subplayerjs.js
@@ -43,9 +43,7 @@ class SubPlayerJS {
         if (!$("link[href='http://fonts.googleapis.com/icon?family=Material+Icons']").length) {
             loadjscssfile("http://fonts.googleapis.com/icon?family=Material+Icons", "css");
         }
-        if (!$("link[href='https://www.citycima.tk/subplayerjs.css']").length) {
-            loadjscssfile("https://www.citycima.tk/subplayerjs.css", "css");
-        }
+       
         if (!$("link[href='https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css']").length) {
             loadjscssfile("https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css", "css");
         }
